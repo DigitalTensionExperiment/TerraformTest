bf2830ba55c4c2fd025829317fb46485153c8818
DigitalTensionExperiment <zwilhelm@Zareens-MacBook-Pro.local>
2017-12-27 15:31:59 -0600

    Launches a simple t2.micro instance (just add your own creds);

--- instance.tf
+++ instance.tf
@@ -1,4 +1,10 @@
-resource "aws_instance" "" {
-  ami = ""
-  instance_type = ""
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region     = "us-east-1"
 }
+
+resource "aws_instance" "testbox0" {
+  ami = "ami-5cd4a126"
+  instance_type = "t2.micro"
+} 
