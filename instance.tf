6508d36f038831d1f43b9f996768431dddcca3fb
DigitalTensionExperiment <zwilhelm@Zareens-MacBook-Pro.local>
2017-12-27 22:16:47 -0600

    splitting up instance between provider and vars.tf;

--- instance.tf
+++ instance.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "us-east-1"
-}
-
 resource "aws_instance" "testbox0" {
-  ami = "ami-5cd4a126"
+  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-} 
+}
